ca48c42251c5b733c830fbf71e2c650ac848d987
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-12 16:41:50 -0400

    Refactored to use hooks

--- src/index.js
+++ src/index.js
@@ -1,60 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
 
-// class-based component
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: "" };
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []);
 
-  componentDidMount() {
-    if (!navigator.geolocation) {
-      console.log("Geolocation not supported by your browser.");
-    } else {
-      navigator.geolocation.getCurrentPosition(
-        position => this.setState({ lat: position.coords.latitude }),
-        // to update state object we called setState!!!!
-        // we did not!!!!!!
-        // this.state.lat = position.coords.latitude
-        err => this.setState({ errorMessage: err.message })
-      );
-    }
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
 
-  componentDidUpdate() {
-    console.log("My component was just updated - it re-rendered!");
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    } else {
-      return <Spinner message="Please accept location request" />;
-    }
-  }
-
-  // React says we have to define render!!
-  render() {
-    return <div>{this.renderContent()}</div>;
-  }
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
-// functional component example
+// // class-based component
 
-// const App = () => {
-//   if (!navigator.geolocation) {
-//     console.log("Geolocation not supported by your browser.");
-//   } else {
-//     navigator.geolocation.getCurrentPosition(
-//       position => console.log(position),
-//       err => console.log(err)
-//     );
+// class App extends React.Component {
+//   state = { lat: null, errorMessage: "" };
+
+//   componentDidMount() {
+//     if (!navigator.geolocation) {
+//       console.log("Geolocation not supported by your browser.");
+//     } else {
+//       navigator.geolocation.getCurrentPosition(
+//         position => this.setState({ lat: position.coords.latitude }),
+//         // to update state object we called setState!!!!
+//         // we did not!!!!!!
+//         // this.state.lat = position.coords.latitude
+//         err => this.setState({ errorMessage: err.message })
+//       );
+//     }
 //   }
 
-//   return <div>Hi there!</div>;
-// };
+//   componentDidUpdate() {
+//     console.log("My component was just updated - it re-rendered!");
+//   }
+
+//   renderContent() {
+//     if (this.state.errorMessage && !this.state.lat) {
+//       return <div>Error: {this.state.errorMessage}</div>;
+//     } else if (!this.state.errorMessage && this.state.lat) {
+//       return <SeasonDisplay lat={this.state.lat} />;
+//     } else {
+//       return <Spinner message="Please accept location request" />;
+//     }
+//   }
+
+//   // React says we have to define render!!
+//   render() {
+//     return <div>{this.renderContent()}</div>;
+//   }
+// }
